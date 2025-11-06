c7ae8e8e6d2b41a559a4564d71ec3a6a02af6bda
Roald Christesen <roald@grain.one>
2025-11-06 19:44:05 +0100

    fix sql for  api/events/types-count endpoint to return coorect count

--- queries/get-events-per-type.sql
+++ queries/get-events-per-type.sql
@@ -1,5 +1,6 @@
 WITH event_data AS (
     SELECT
+        ed.id AS event_date_id,
         ed.event_id
     FROM {{schema}}.event_date ed
     JOIN {{schema}}.event e
@@ -7,11 +8,12 @@ ON ed.event_id = e.id
     AND e.release_status_id >= 2
     {{event-date-conditions}}
     ),
+
     event_counts AS (
 SELECT
     et.type_id AS id,
     et.name,
-    COUNT(DISTINCT ed.event_id) AS count
+    COUNT(DISTINCT ed.event_date_id) AS count
 FROM {{schema}}.event_type et
     LEFT JOIN {{schema}}.event_type_links etl
 ON et.type_id = etl.type_id
@@ -19,10 +21,10 @@ ON et.type_id = etl.type_id
     ON ed.event_id = etl.event_id
 WHERE et.iso_639_1 = $1
     {{conditions}}
-    {{limit}}
 GROUP BY et.type_id, et.name
-HAVING COUNT(DISTINCT ed.event_id) > 0
+HAVING COUNT(DISTINCT ed.event_date_id) > 0
+     {{limit}}
     )
 SELECT *
 FROM event_counts
-ORDER BY name
+ORDER BY name;
